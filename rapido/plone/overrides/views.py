373b0fd35423493ccf394978f1f683ab4d6b7370
ebrehault <ebrehault@gmail.com>
2013-11-01 11:03:32 +0100

    overrides document view

--- rapido/plone/overrides/views.py
+++ rapido/plone/overrides/views.py
@@ -5,3 +5,7 @@ from rapido.core.browser import views as core
 class OpenForm(core.OpenForm):
 
     template = ViewPageTemplateFile('templates/openform.pt')
+
+class DocumentView(core.DocumentView):
+
+    template = ViewPageTemplateFile('templates/opendocument.pt')
