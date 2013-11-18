8e387f9d824a55a39ca732612c696bcd734e1c97
ebrehault <ebrehault@gmail.com>
2013-11-18 19:51:33 +0100

    clean up

--- rapido/plone/database.py
+++ rapido/plone/database.py
@@ -18,25 +18,10 @@ from rapido.core.interfaces import IDatabasable
 from rapido.plone import MessageFactory as _
 
 
-# Interface class; used to define content-type schema.
-
 class IDatabase(form.Schema, IImageScaleTraversable):
     """
     Rapido database
     """
-
-    # If you want a schema-defined interface, delete the model.load
-    # line below and delete the matching file in the models sub-directory.
-    # If you want a model-based interface, edit
-    # models/database.xml to define the content type.
-
-    form.model("models/database.xml")
-
-
-# Custom content-type class; objects created for this content type will
-# be instances of this class. Use this class to add content-type specific
-# methods and properties. Put methods that are mainly useful for rendering
-# in separate view classes.
 
 class Database(Container):
     grok.implements(IDatabase, IDatabasable)
@@ -51,24 +36,3 @@ class Database(Container):
 
     def url(self):
         return self.absolute_url()
-
-
-# View class
-# The view will automatically use a similarly named template in
-# database_templates.
-# Template filenames should be all lower case.
-# The view will render when you request a content object with this
-# interface with "/@@sampleview" appended.
-# You may make this the default view for content objects
-# of this type by uncommenting the grok.name line below or by
-# changing the view class name and template filename to View / view.pt.
-
-class SampleView(grok.View):
-    """ sample view class """
-
-    grok.context(IDatabase)
-    grok.require('zope2.View')
-
-    # grok.name('view')
-
-    # Add view methods here
--- rapido/plone/field.py
+++ rapido/plone/field.py
@@ -51,33 +51,6 @@ class IField(form.Schema, IImageScaleTraversable):
             required=True,
         )
 
-# Custom content-type class; objects created for this content type will
-# be instances of this class. Use this class to add content-type specific
-# methods and properties. Put methods that are mainly useful for rendering
-# in separate view classes.
 
 class Field(Container):
     grok.implements(IField)
-
-    # Add your class methods and properties here
-
-
-# View class
-# The view will automatically use a similarly named template in
-# field_templates.
-# Template filenames should be all lower case.
-# The view will render when you request a content object with this
-# interface with "/@@sampleview" appended.
-# You may make this the default view for content objects
-# of this type by uncommenting the grok.name line below or by
-# changing the view class name and template filename to View / view.pt.
-
-class SampleView(grok.View):
-    """ sample view class """
-
-    grok.context(IField)
-    grok.require('zope2.View')
-
-    # grok.name('view')
-
-    # Add view methods here
