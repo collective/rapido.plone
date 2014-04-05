5b729df7f6cc08d77ed84813cbfd8b0baa11636b
ebrehault <ebrehault@gmail.com>
2014-04-05 21:02:13 +0200

    allow all HTTP methods for the REST api

--- rapido/plone/browser/rest.py
+++ rapido/plone/browser/rest.py
@@ -12,7 +12,9 @@ class Api(BrowserView):
     def __init__(self, context, request):
         self.context = context
         self.request = request
-        self.query = "get"
+        self.db = IDatabase(self.context)
+        self.query = None
+        self.method = self.request.method
         self.doc = None
         self.error = None
 
@@ -21,21 +23,46 @@ class Api(BrowserView):
         return json.dumps(result)
 
     def publishTraverse(self, request, name):
-        if name in ["_update", "_create", "_delete", "_search"]:
+        if self.method == "GET" and name == "_search":
+            self.query = name
+            return self
+        if self.method == "POST" and name in ["_update", "_create", "_delete"]:
             self.query = name
             return self
 
-        doc = IDatabase(self.context).get_document(name)
+        doc = self.db.get_document(name)
         if not doc:
             raise NotFound(self, name, request)
         self.doc = doc
         return self
 
     def render(self):
-        if self.query == "get":
-            return self.json_response(self.doc.items())
-        else:
-            return self.json_response('Not implemented')
+        try:
+            if self.method == "GET" and not self.query:
+                return self.json_response(self.doc.items())
+            elif self.method == "PUT" or (self.method == "POST" and self.query == "_create"):
+                return self.json_response({'error': 'Not implemented'})
+            elif self.method == "DELETE" or (self.method == "POST" and self.query == "_delete"):
+                self.db.delete_document(doc=self.doc)
+                return self.json_response({'success': 'deleted'})
+            elif self.method == "PATCH" or (self.method == "POST" and self.query == "_update"):
+                return self.json_response({'error': 'Not implemented'})
+            else:
+                return self.json_response({'error': 'Not allowed'})
+        except Exception, e:
+            return self.json_response({'error': str(e)})
+
+
 
     def __call__(self):
         return self.render()
+
+    def __contains__(self, name):
+        # When processing other methods than GET and POST, ZPublisher tyies to
+        # make sure the requested resource exists in the parent. So we just 
+        # retend that any part of the url path is an existing element.
+        # That's qute hacky :)
+        if name in self.request.URL.split('/'):
+            return True
+        else:
+            return False
