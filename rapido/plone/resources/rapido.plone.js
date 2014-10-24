5a8d6e3bc4e5829a8176c9a45c33d41b733a622b
Eric BREHAULT <ebrehault@gmail.com>
2014-10-24 16:13:35 +0200

    add ace editor

---
+++ rapido/plone/resources/rapido.plone.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    if($('textarea#form-widgets-code').length > 0) {
+        var editor = ace.edit("editor");
+        textarea = $('textarea#form-widgets-code');
+        textarea.hide();
+        editor.setValue(textarea.val());
+        editor.setTheme("ace/theme/monokai");
+        editor.getSession().setMode("ace/mode/python");
+        editor.commands.addCommand({
+            name: 'save',
+            bindKey: {win: 'Ctrl-S',  mac: 'Command-S'},
+            exec: function(editor) {
+                $('#form').submit();
+            },
+            readOnly: false
+        });
+        $('#form').submit(function() {
+            textarea.val(editor.getValue());
+        })
+    }
+})
