2dc757f1cf94976c8c7f00f879a8096a3401d3cf
Eric BREHAULT <ebrehault@gmail.com>
2015-06-28 19:43:21 +0200

    json view

--- src/rapido/plone/browser/views.py
+++ src/rapido/plone/browser/views.py
@@ -1,3 +1,4 @@
+import json
 from Products.Five.browser import BrowserView
 from zope.interface import implements
 from zope.publisher.interfaces import IPublishTraverse
@@ -32,12 +33,30 @@ class RapidoView(BrowserView):
         app = get_app(app_id, self.request)
         return app.process(self.method, directive, obj_id, action)
 
+    def json(self, path=None):
+        if not path:
+            path = self.path[1:]
+        app_id = path[0]
+        directive = path[1]
+        if len(path) > 2:
+            obj_id = path[2]
+        else:
+            obj_id = None
+        app = get_app(app_id, self.request)
+        return app.json(self.method, self.request, directive, obj_id)
+
     def __call__(self):
         if self.path[0] == 'view':
             # this is neutral, we just return the default content view
             # but it gives the opportunity to create specific pseudo views
             # via our Diazo rules.xml
             return self.context()
-        result = self.content()
-        self.request.response.setHeader('X-Theme-Disabled', '1')
-        return result
+        elif self.path[0] == 'json':
+            result = self.json()
+            self.request.response.setHeader('X-Theme-Disabled', '1')
+            self.request.response.setHeader('content-type', 'application/json')
+            return json.dumps(result)
+        else:
+            result = self.content()
+            self.request.response.setHeader('X-Theme-Disabled', '1')
+            return result
