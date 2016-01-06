6b32e57904467860178806cb2f66bbf2ee6e02f0
Eric BREHAULT <ebrehault@gmail.com>
2016-01-06 23:09:46 +0100

    Back to development: 1.0.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (
 
 setup(
     name='rapido.plone',
-    version='1.0.1',
+    version='1.0.2.dev0',
     description="Rapido for Plone",
     long_description=long_description,
     # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
