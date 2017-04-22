111b008715b57e804bfa0565bd0870cf1a46b78e
Eric BREHAULT <ebrehault@gmail.com>
2017-04-22 12:18:29 +0200

    Preparing release 1.1.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (
 
 setup(
     name='rapido.plone',
-    version='1.1.1.dev0',
+    version='1.1.1',
     description="Rapido for Plone",
     long_description=long_description,
     # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
